ca75d5b1771140bf721324b2666e40a07a9c1cfb
Isack Ramirez <isack.ramirez1@gmail.com>
2022-02-08 16:23:55 -0600

    initial commit, file structure and basic page template

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Feed from './pages/Feed'
+
+function App() {
+  return (
+    <div className="App">
+
+      <Feed/>
+   
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Feed.js
@@ -0,0 +1,37 @@
+import React from "react";
+import styled from "styled-components";
+
+const Feed = () => {
+  return <>
+    
+    <styledFeed>
+        <Heading>Feed:</Heading>
+    </styledFeed>
+
+  </>;
+};
+
+const styledFeed = styled.div`
+  min-height: 100vh;
+  width: 100vw;
+  background-color: white;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+const Heading = styled.h1`
+  font-size: clamp(3rem, 5vw, 7vw);
+  color: #eee;
+  font-weight: 700;
+  margin: 0;
+  padding: 0;
+
+  user-select: none;
+  -webkit-user-select: none;
+  -khtml-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+`;
+export default Feed;
